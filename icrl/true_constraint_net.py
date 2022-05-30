a8ac52bb5c41d14e501f5c469ce588458d6051d1
Mattijs Baert <mattijs.baert@ugent.be>
2022-05-30 07:46:44 +0200

    add true cost function for JTL env

--- icrl/true_constraint_net.py
+++ icrl/true_constraint_net.py
@@ -23,14 +23,14 @@ def get_true_cost_function(env_id):
         return partial(wall_behind_and_infront, -3, +3)
     elif env_id == "PointCircleTestBack-v0":
         return partial(wall_behind, -3)
-    elif env_id in ["CDD2B-v0", "CC2B-v0", "CDD3B-v0"]:
+    elif env_id in ["CDD2B-v0", "CC2B-v0", "CDD3B-v0", "DD2B-v0"]:
         dummy_env = gym.make(env_id)
         return partial(bridges, dummy_env)
     elif env_id == "CLGW-v0":
         return lap_grid_world
-    elif env_id == "CSW-v0":
+    elif env_id == "CJTL-v0":
         dummy_env = gym.make(env_id)
-        return partial(scobee_world, dummy_env)
+        return partial(junction_traffic_lights, dummy_env)
     elif env_id in ["AntTest-v0", 'HalfCheetahTest-v0', 'Walker2dTest-v0', 'SwimmerTest-v0']:
         return partial(torque_constraint, 0.5)
     else:
@@ -81,33 +81,32 @@ def bridges(env, obs, acs):
     cost = []
     for ob, ac in zip(obs, acs):
         # Need to unnormalize obs
-        #ob = unnormalize(env, ob)
-        #
+        ob = unnormalize(env, ob)
+
         # originally this was "if 'action_map_dict' in env.__dict__ but this did not work for DD2B env
-        # if 'action_map_dict' in env.env.__dict__:
-        #    # For discrete classes
-        #    ac = env.action_map_dict[int(ac)]
-        #    next_ob = np.around(ob+ac, 6)
-        # else:
-        #    # For continuous classes
-        #    ac = np.clip(ac, a_min=env.action_space.low,
-        #                 a_max=env.action_space.high)
-        #    ori = ob[2] + ac[1]
-        #    dx = math.cos(ori) * ac[0]
-        #    dy = math.sin(ori) * ac[0]
-        #
-        #    next_ob = ob.copy()
-        #    next_ob[0] = np.clip(ob[0] + dx, -env.size, env.size)
-        #    next_ob[1] = np.clip(ob[1] + dy, -env.size, env.size)
-        #
-        #    ob = next_ob[:2]
-        #    next_ob = next_ob[:2]
-        #
-        # if ce_utils.in_regions(ob, next_ob, env.constraint_regions):
-        #    cost += [1]
-        # else:
-        #    cost += [0]
-        cost += [0]
+        if 'action_map_dict' in env.env.__dict__:
+            # For discrete classes
+            ac = env.action_map_dict[int(ac)]
+            next_ob = np.around(ob+ac, 6)
+        else:
+            # For continuous classes
+            ac = np.clip(ac, a_min=env.action_space.low,
+                         a_max=env.action_space.high)
+            ori = ob[2] + ac[1]
+            dx = math.cos(ori) * ac[0]
+            dy = math.sin(ori) * ac[0]
+
+            next_ob = ob.copy()
+            next_ob[0] = np.clip(ob[0] + dx, -env.size, env.size)
+            next_ob[1] = np.clip(ob[1] + dy, -env.size, env.size)
+
+            ob = next_ob[:2]
+            next_ob = next_ob[:2]
+
+        if ce_utils.in_regions(ob, next_ob, env.constraint_regions):
+            cost += [1]
+        else:
+            cost += [0]
 
     cost = np.reshape(np.array(cost), cost_shape)
 
@@ -123,21 +122,32 @@ def lap_grid_world(obs, acs):
             cost += [0]
     return np.array(cost)
 
+def junction_traffic_lights(env, obs, acs):
+    obs = copy.deepcopy(obs)
+    acs = copy.deepcopy(acs)
+    if len(obs.shape) > 2:
+        batch_size, n_envs, _ = obs.shape
+        cost_shape = (batch_size, n_envs)
+        obs = np.reshape(obs, (batch_size*n_envs, -1))
+        acs = np.reshape(acs, (batch_size*n_envs, -1))
+    else:
+        cost_shape = (obs.shape[0])
 
-def scobee_world(env, obs, acs):
     cost = []
-    for ob in obs:
-        # shape of ob is (2), when batch processing: (1, 2)
-        # if(ob.shape[0] == 2):
+    for ob, ac in zip(obs, acs):
+        # Need to unnormalize obs
         ob = unnormalize(env, ob)
-        x, y = env.idx_to_xy(ob)
-        if(x == 4) and (y == 0):
+        ac = env.action_map_dict[int(ac)]
+        next_ob = np.around(ob+ac, 6)
+
+        if ce_utils.in_regions(ob, next_ob, env.constraint_regions):
             cost += [1]
         else:
             cost += [0]
 
-    return np.array(cost)
+    cost = np.reshape(np.array(cost), cost_shape)
 
+    return cost
 # ============================================================================
 # Utils
 # ============================================================================
