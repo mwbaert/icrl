37fed9270717067030dc1fdd9ffe43e1e3a0db0a
Mattijs Baert <mattijs.baert@ugent.be>
2022-06-16 15:13:56 +0200

    update docker_init.sh

--- docker_init.sh
+++ docker_init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-python3.8 -V 
+echo 'alias python=/usr/bin/python3.8' > ~/.bashrc
+. ~/.bashrc
+python -V 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/jovyan/.mujoco/mujoco210/bin
 pip install -e ./custom_envs
+pip install torch
 wandb login fa44fb586bc0ae1f03502cb1b6268f3b916b163b
