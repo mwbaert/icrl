ef7ea77354531d8e58b561dadf107c6ee528caa3
Mattijs Baert <mattijs.baert@ugent.be>
2022-06-17 11:08:16 +0200

    update docker

--- docker_init.sh
+++ docker_init.sh
@@ -3,7 +3,7 @@
 python3.8 -V
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/jovyan/.mujoco/mujoco210/bin
 pip3.8 install -e ./custom_envs
-pip3.8 install torch pandas tqdm imageio
+pip3.8 install torch pandas tqdm imageio wandb
 wandb login fa44fb586bc0ae1f03502cb1b6268f3b916b163b
 
 #"bash -c \"git clone https://github.com/mwbaert/icrl.git; cd icrl; chmod +x docker_init.sh; ./docker_init.sh; python run_me.py icrl --config_file config_icrl_logic_jtl.json -ws True --n_iters 20 --l2_coeff 1e-5\""
