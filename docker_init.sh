b6e9a2b7dd775b5206412e112df661cd4f9e57e5
Mattijs Baert <mattijs.baert@ugent.be>
2022-06-27 16:27:47 +0200

    update docker_init.sh

--- docker_init.sh
+++ docker_init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 python3.8 -V
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/jovyan/.mujoco/mujoco210/bin
-pip3.8 install -e ./custom_envs
 pip3.8 install torch pandas tqdm imageio wandb matplotlib mpl-scatter-density glfw mujoco mujoco-py
 pip3.8 install --upgrade gym==0.15.7
+pip3.8 install -e ./custom_envs
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/jovyan/.mujoco/mujoco210/bin
 wandb login fa44fb586bc0ae1f03502cb1b6268f3b916b163b
 
 #"bash -c \"git clone https://github.com/mwbaert/icrl.git; cd icrl; chmod +x docker_init.sh; ./docker_init.sh; python run_me.py icrl --config_file config_icrl_logic_jtl.json -ws True --n_iters 20 --l2_coeff 1e-5\""
--- submit.sh
+++ submit.sh
@@ -1,4 +1,4 @@
-gpulab-cli --cert="../../../login_ilabt_imec_be_mwbaert@ugent.be.pem" --debug submit --project=phd_mattijs < job.json
+gpulab-cli --cert="../../../login_ilabt_imec_be_mwbaert@ugent.be.pem" --debug submit --project=phd_mattijs < job_cpg.json
 
 echo "current commit:"
 git log -1
